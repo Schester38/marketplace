d17b40068bc3cf88d0677baeffe8701636fa6a7e
Jean Arthur <jeana7010@gmail.com>
2026-08-08 18:19:22 +0100

    Retirer le lien Verone du bandeau (page accessible uniquement par URL)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,7 +13,6 @@ export default function Navbar({ onLogout }) {
       <nav>
         <Link to="/">Produits</Link>
         <Link to="/vitrine-offre">Vitrine d'offre</Link>
-        <Link to="/verone">Verone</Link>
         {!user && <Link to="/login">Connexion</Link>}
         {!user && <Link to="/register" className="btn btn-primary">Créer un compte</Link>}
         {user && user.role === 'shop' && <Link to="/shop">Ma boutique</Link>}
