a8ac71cf4f16adee252b731f138c3a85fbcdda0b
Jean Arthur <jeana7010@gmail.com>
2026-08-14 08:54:52 +0100

    Service worker: installation resiliente (Promise.allSettled) pour que l'activation ne soit pas bloquee par un fichier en echec de telechargement sur reseau instable

--- client/public/sw.js
+++ client/public/sw.js
@@ -3,7 +3,10 @@ const APP_SHELL = ['/', '/manifest.webmanifest', '/manifest-verone.webmanifest',
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => Promise.allSettled(APP_SHELL.map((url) => cache.add(url))))
+      .then(() => self.skipWaiting())
   );
 });
 
